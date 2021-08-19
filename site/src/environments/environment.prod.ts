38754b33ac59de16ab7f4f80491f830df86ca5d2
Arthur Ferreira <arthur_f.oliveira@hotmail.com>
2021-08-19 01:10:44 -0300

    fix: keys prod

--- site/src/environments/environment.prod.ts
+++ site/src/environments/environment.prod.ts
@@ -3,3 +3,5 @@ export const environment = {
 };
 
 export const API_URL = 'http://localhost:3333/';
+export const KEY_MAILBOX = 'f20f7ae318c34b92ee6a685fac758feb';
+export const PASS_AUTH = 'TrabalheNaSaipos';
